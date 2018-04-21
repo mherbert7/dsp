ac37689a3c9b7dafadcd6fcac0c3b00750282c71
Marcus <marcusherbert7@gmail.com>
2018-04-21 22:41:05 +1000

    Tidied up fft leakge script

--- fft/fft_leakage.py
+++ fft/fft_leakage.py
@@ -7,19 +7,20 @@ Created on Sat Apr 21 21:52:03 2018
 
 import numpy as np
 import pylab as plt
-from matplotlib import collections as matcoll
 
-#FFT leakage
+#FFT Leakage Script
+#If m is not an integer, then leakage will be observed
 
-complex_in = True
+complex_in = False
 plot = True
 
 N = 64
-fs = 1
+fs = 0.53456257 * np.exp(np.pi)
 ts = 1/fs
 A0 = 1
 
 t = np.linspace(0, (N - 1) * ts, N)
+#t = np.arange(0, N * ts, ts) #<- This is an equivalent way to generate t
 
 m = 37
 f = m * fs / N
@@ -32,12 +33,14 @@ else:
 
 x *= A0
 
-f_out = np.abs(np.fft.fft(x))
+f_out = 20 * np.log10(np.abs(np.fft.fft(x)))
 frequencies = np.fft.fftfreq(N, d=ts)
 
 if(plot):
-    #plt.plot(frequencies, f_out)
     plt.stem(f_out, markerfmt='b.')
+    plt.title("FFT Output")
+    plt.xlabel("Frequency index")
+    plt.ylabel("Magnitude (dB)")
     
     plt.show()
     
